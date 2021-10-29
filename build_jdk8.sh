e4f3ed85bbdf39077fe2763c17d5d5b2714b9400
Joseph <mate.joseph@gmail.com>
2021-10-28 21:19:04 -0400

    fixes styles

--- build_jdk8.sh
+++ build_jdk8.sh
@@ -1,6 +1,6 @@
 args="--inputSourceDirectory ../jdk8/jdk/src/share/classes"
 args="$args --outputDirectory docs/jdk8"
-args="$args --webPathToCssFile /OdinCodeBrowser/styles.css"
+args="$args --webPathToCssFile /OdinCodeBrowser/css/styles.css"
 args="$args --webPathToSourceHtmlFiles /OdinCodeBrowser/jdk8"
 mvn install exec:java \
   -Dexec.mainClass=Main \
