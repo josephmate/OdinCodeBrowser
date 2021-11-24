458387d3e41f8374b3de9ae9e85b35fc922271d1
Joseph <mate.joseph@gmail.com>
2021-11-23 22:10:32 -0500

    Added jdk11 and jdk17

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,8 @@
+# only build the currently supported JDKs
+# we can add the short term releases when requested
 ./build_jdk8.sh
+./build_jdk11.sh
+./build_jdk17.sh
 ./build_commons-lang3_3.11.sh
 ./build_commons-collections_4.4.sh
 ./build_commons-text_1.9.sh
